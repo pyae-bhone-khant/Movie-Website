55874b6edff709a70f3bad41869925d88e3ecfab
pyae-bhone-khant <pyaebhonen4@gmail.com>
2025-09-16 13:43:42 +0630

    Describe change

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// export default defineConfig({
-//   plugins: [react() , tailwindcss()],
-//   server: {
-//     open: true,
-//   }
-// }) 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react() , tailwindcss()],
   server: {
     open: true,
-    proxy: {
-      // Proxy API requests to FreeToGame
-      '/api': {
-        target: 'https://www.freetogame.com/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-})
-
+  }
+}) 
